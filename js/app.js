becaf2f3d061ad7d7c5f64fec3d16b529e3c850d
The73756 <the73756the@gmail.com>
2022-05-18 21:17:17 +0900

    master

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const sliderMain = new Swiper('.slider-main', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	sensitivity: 10,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+});
+
+const sliderBg = new Swiper('.slider-bg', {
+	centeredSlides: true,
+	parallax: true,
+	sensitivity: 10,
+	slidesPerView: 3.5,
+});
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll('.slider__item').forEach(item => {  
+	item.addEventListener('click', event =>{
+		new GraphModal().open('first');
+	})
+});
